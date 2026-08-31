77330d480a9e4eae7966b1fa5f334aa4460bacb4
Jaden Chuah <jadenchuah@outlook.com>
2026-08-31 18:16:10 +1000

    Fix the diagnostic

--- _test_startup.py
+++ _test_startup.py
@@ -4,48 +4,83 @@
 (well more importantly, _where_ it spends that 1 minute, and how to mitigate that)
 """
 
-print('script::start')
-
 import time
 
-last_time = time.perf_counter()
+
+input('script::start> ')
+
+# Overall script timer starts after the user presses Enter.
+script_start = time.perf_counter()
+
 
 print('import::constants::start')
+section_start = time.perf_counter()
+
 from constants import *
-current_time = time.perf_counter()
-print('import::constants::end - total: {:.4f}s'.format(current_time - last_time))
+
+section_end = time.perf_counter()
+print('import::constants::end - total: {:.4f}s'.format(
+    section_end - section_start
+))
 
 print()
+
 
 print('import::utils::start')
+section_start = time.perf_counter()
+
 from utils import *
-current_time = time.perf_counter()
-print('import::utils::end - total: {:.4f}s'.format(current_time - last_time))
+
+section_end = time.perf_counter()
+print('import::utils::end - total: {:.4f}s'.format(
+    section_end - section_start
+))
 
 print()
+
 
 print('import::robot::start')
+section_start = time.perf_counter()
+
 from robot import *
-current_time = time.perf_counter()
-print('import::robot::end - total: {:.4f}s'.format(current_time - last_time))
+
+section_end = time.perf_counter()
+print('import::robot::end - total: {:.4f}s'.format(
+    section_end - section_start
+))
 
 print()
+
 
 print('import::map::start')
+section_start = time.perf_counter()
+
 from map import *
-current_time = time.perf_counter()
-print('import::map::end - total: {:.4f}s'.format(current_time - last_time))
+
+section_end = time.perf_counter()
+print('import::map::end - total: {:.4f}s'.format(
+    section_end - section_start
+))
 
 print()
+
 
 print('import::solver::start')
+section_start = time.perf_counter()
+
 from solver import *
-current_time = time.perf_counter()
-print('import::solver::end - total: {:.4f}s'.format(current_time - last_time))
+
+section_end = time.perf_counter()
+print('import::solver::end - total: {:.4f}s'.format(
+    section_end - section_start
+))
 
 print()
 
+
 print('robot::start')
+section_start = time.perf_counter()
+
 robot = Robot()
 robot.init()
 
@@ -53,11 +88,16 @@ robot.log()
 
 robot.shutdown()
 
-current_time = time.perf_counter()
-print('robot::end - total: {:.4f}s'.format(current_time - last_time))
+section_end = time.perf_counter()
+print('robot::end - total: {:.4f}s'.format(
+    section_end - section_start
+))
 
 print()
 
-current_time = time.perf_counter()
-print('script::end - total: {:.4f}s'.format(current_time - last_time))
+
+script_end = time.perf_counter()
+print('script::end - total: {:.4f}s'.format(
+    script_end - script_start
+))
 
