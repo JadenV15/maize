13c4971e4add5ad1e1d2cceae7c80320dea2f6bd
Jaden Chuah <jadenchuah@outlook.com>
2026-09-05 09:30:27 +1000

    Change to enter press

--- button.py
+++ button.py
@@ -8,40 +8,40 @@ from ev3dev2.button import Button
 from constants import BUTTON_POLL_INTERVAL_MS
 
 __all__ = [
-    'backspace_pressed',
-    'reset_backspace',
+    'enter_pressed',
+    'reset_enter',
     'wait_for_enter_press'
 ]
 
 btn = Button()
 
-_has_backspace_pressed = False # type: bool
+_has_enter_pressed = False # type: bool
 
-def backspace_pressed() -> bool:
-    """Has the backspace button been pressed?"""
-    return _has_backspace_pressed
+def enter_pressed() -> bool:
+    """Has the enter button been pressed?"""
+    return _has_enter_pressed
 
-def on_backspace_press(state):
-    global _has_backspace_pressed
+def on_enter_press(state):
+    global _has_enter_pressed
     if state:
-        _has_backspace_pressed = True
+        _has_enter_pressed = True
 
-btn.on_backspace = on_backspace_press # type: ignore
+btn.on_enter = on_enter_press # type: ignore
 
-def poll_backspace():
+def poll_enter():
     while True:
         btn.process()
         time.sleep(BUTTON_POLL_INTERVAL_MS / 1000)
 
 # bg thread
 # this thread runs for the entirety of the program
-t = Thread(target=poll_backspace, daemon=True)
+t = Thread(target=poll_enter, daemon=True)
 t.start()
 
-def reset_backspace():
-    """Reset backspace_pressed() to false"""
-    global _has_backspace_pressed
-    _has_backspace_pressed = False
+def reset_enter():
+    """Reset enter_pressed() to false"""
+    global _has_enter_pressed
+    _has_enter_pressed = False
 
 def wait_for_enter_press():
     """Wait until enter button is pressed"""
